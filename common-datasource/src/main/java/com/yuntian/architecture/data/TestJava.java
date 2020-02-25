96a5a25c29a19b5ee54277d10949e238a6e10a37
yuntian <944610721@qq.com>
2020-02-25 18:12:37 +0800

    增加类满足打包

--- common-datasource/src/main/java/com/yuntian/architecture/data/TestJava.java
+++
@@ -1,9 +0,0 @@
-package com.yuntian.architecture.data;
-
-/**
- * @Auther: yuntian
- * @Date: 2020/2/25 0025 18:09
- * @Description:
- */
-public class TestJava {
-}
---
+++ common-datasource/src/main/java/com/yuntian/architecture/datasource/TestDatasource.java
@@ -0,0 +1,9 @@
+package com.yuntian.architecture.datasource;
+
+/**
+ * @Auther: yuntian
+ * @Date: 2020/2/25 0025 18:12
+ * @Description:
+ */
+public class TestDatasource {
+}
